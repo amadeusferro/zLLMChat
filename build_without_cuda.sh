223fc1807306beb415fe958c5e04a2327a0a07c2
Amadeus Ferro <amadeus.ferro55@gmail.com>
2025-07-12 22:05:07 -0300

    Locking llama.cpp to a commit from May 2025

--- build_without_cuda.sh
+++ build_without_cuda.sh
@@ -9,7 +9,7 @@ mkdir -p build || { echo "Failed to create build directory"; exit 1; }
 cd build || { echo "Failed to change directory to build"; exit 1; }
 
 # llama.cpp has seen few changes recently, so I locked it to a commit from May 2025
-git checkout 33983057d0f578aca74ba15eccc3de9c267a5ff6
+git checkout 1d36b3670b285e69e58b9d687c770a2a0a192194
 
 cmake .. \
   -DLLAMA_CUBLAS=OFF || { echo "Failed to configure CMake"; exit 1; }
